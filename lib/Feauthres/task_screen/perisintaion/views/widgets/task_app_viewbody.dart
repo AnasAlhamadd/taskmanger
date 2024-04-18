0e69b33d40f3d037601a84a81f588847cb5d9542
anasAlhamad <awatan385@gmail.com>
2024-04-18 03:52:42 +0300

    refactor custom container rname task componets

--- lib/Feauthres/task_screen/perisintaion/views/widgets/task_app_viewbody.dart
+++ lib/Feauthres/task_screen/perisintaion/views/widgets/task_app_viewbody.dart
@@ -1,6 +1,7 @@
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:untitledtaskmanger/Feauthres/task_screen/perisintaion/views/widgets/task_componets.dart';
 import 'package:untitledtaskmanger/core/helper/constant.dart';
 import 'package:untitledtaskmanger/core/helper/styles.dart';
 import 'package:intl/intl.dart';
@@ -73,69 +74,7 @@ class TaskAppBodyView extends StatelessWidget {
                 const SizedBox(
                   height: 25,
                 ),
-                Container(
-                  height: 130,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Row(
-                      children: [
-                        //Custom title and time and Icon
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            //text Task
-                            Text(
-                              'Task 1',
-                              style: Styles().textstyle28,
-                            ),
-                            //Custom time and Icon
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.alarm,
-                                  size: 30,
-                                ),
-                                const SizedBox(
-                                  width: 8.0,
-                                ),
-                                Text(
-                                  '09:33 PM - 09:48 PM',
-                                  style: Styles().textStyle18,
-                                ),
-                              ],
-                            ),
-                            //Text title
-                            Text(
-                              'Learn Dart',
-                              style: Styles().textstyle28,
-                            )
-                          ],
-                        ),
-                        const Spacer(),
-                        //custom Driveer and Todo Text
-                        const VerticalDivider(
-                          color: Colors.white,
-                          indent: 30,
-                          endIndent: 30,
-                        ),
-                        RotatedBox(
-                          quarterTurns: 3,
-                          child: Text(
-                            'TODO',
-                            style: Styles()
-                                .textStyle18
-                                .copyWith(fontFamily: 'sans'),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                const TaskComponets(),
               ],
             ),
           ),
---
+++ lib/Feauthres/task_screen/perisintaion/views/widgets/task_componets.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:untitledtaskmanger/core/helper/styles.dart';
+
+class TaskComponets extends StatelessWidget {
+  const TaskComponets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            //Custom title and time and Icon
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //text Task
+                Text(
+                  'Task 1',
+                  style: Styles().textstyle28,
+                ),
+                //Custom time and Icon
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.alarm,
+                      size: 30,
+                    ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      '09:33 PM - 09:48 PM',
+                      style: Styles().textStyle18,
+                    ),
+                  ],
+                ),
+                //Text title
+                Text(
+                  'Learn Dart',
+                  style: Styles().textstyle28,
+                )
+              ],
+            ),
+            const Spacer(),
+            //custom Driveer and Todo Text
+            const VerticalDivider(
+              color: Colors.white,
+              indent: 30,
+              endIndent: 30,
+            ),
+            RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                'TODO',
+                style: Styles().textStyle18.copyWith(fontFamily: 'sans'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
